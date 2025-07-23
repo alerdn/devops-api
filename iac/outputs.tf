4181794582bf43def18e55a8e5274bf2b2c11482
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2025-07-23 09:45:49 -0400

    wip: configurando backend

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,11 +1,11 @@
 output "s3_bucket_name" {
-  value = module.s3.bucket_domain_name
-  sensitive = false
+  value       = module.s3.bucket_domain_name
+  sensitive   = false
   description = "Nome do domínio do bucket S3"
 }
 
 output "cdn_domain_name" {
-  value = module.cdn.cdn_domain_name
-  sensitive = false
+  value       = module.cdn.cdn_domain_name
+  sensitive   = false
   description = "Nome de domínio do CloudFront"
 }
--- iac/providers.tf
+++ iac/providers.tf
@@ -5,9 +5,26 @@ terraform {
       version = "6.4.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "devops-api-state-bucket-tf"
+    region  = "us-east-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+    profile = "admin"
+  }
 }
 
 provider "aws" {
   profile = "admin"
   region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  # Não podemos deletar esse bucket, pois ele é usado para armazenar o estado do Terraform
+  lifecycle {
+    prevent_destroy = true
+  }
 }
