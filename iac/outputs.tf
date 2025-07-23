5ce58c961cdf33da2ab0967258f981d295984290
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2025-07-23 09:55:44 -0400

    wip: limpando projeto

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,11 +1,13 @@
-output "s3_bucket_name" {
-  value       = module.s3.bucket_domain_name
-  sensitive   = false
-  description = "Nome do domínio do bucket S3"
-}
+# Outputs são apenas logs
 
-output "cdn_domain_name" {
-  value       = module.cdn.cdn_domain_name
-  sensitive   = false
-  description = "Nome de domínio do CloudFront"
-}
+# output "s3_bucket_name" {
+#   value       = module.s3.bucket_domain_name
+#   sensitive   = false
+#   description = "Nome do domínio do bucket S3"
+# }
+
+# output "cdn_domain_name" {
+#   value       = module.cdn.cdn_domain_name
+#   sensitive   = false
+#   description = "Nome de domínio do CloudFront"
+# }
