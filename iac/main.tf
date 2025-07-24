7acc3cd5878d468c2f998827b299aa07ca1d6646
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2025-07-24 10:34:20 -0400

    wip: usando variaveis

--- iac/main.tf
+++ iac/main.tf
@@ -1,6 +1,6 @@
 module "s3" {
   source         = "./modules/s3"
-  s3_bucket_name = "devops-api-iac"
+  s3_bucket_name = var.s3_bucket_name
 
   tags = {
     Iac     = true
@@ -12,7 +12,7 @@ module "cdn" {
   source             = "./modules/cloudfront"
   origin_id          = module.s3.bucket_id
   bucket_domain_name = module.s3.bucket_website_domain_name
-  price_class        = "PriceClass_200"
+  price_class        = var.cdn_price_class
 
   tags = {
     Iac     = true
--- iac/variables.tf
+++ iac/variables.tf
@@ -3,3 +3,15 @@ variable "state_bucket" {
   default     = "devops-api-state-bucket-tf"
   description = "Bucket com o estado"
 }
+
+variable "s3_bucket_name" {
+  type        = string
+  default     = "devops-api-iac"
+  description = "Nome do bucket S3 para armazenar os arquivos estáticos."
+}
+
+variable "cdn_price_class" {
+  type        = string
+  default     = "PriceClass_200"
+  description = "Classe de preço para a distribuição do CloudFront. Pode ser PriceClass_100, PriceClass_200 ou PriceClass_All."
+}
