9fe1b868dcc80df89b06c231325e538cd57e73f0
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2025-08-06 16:07:40 -0400

    wip: subindo aplicação go no runner para teste

--- order-service-go/main.go
+++ order-service-go/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"github.com/alerdn/order-service/app/order"
-	"github.com/alerdn/order-service/app/product"
-	"github.com/alerdn/order-service/app/user"
-	"github.com/alerdn/order-service/database"
-	"github.com/alerdn/order-service/messenger"
+	// "github.com/alerdn/order-service/app/order"
+	// "github.com/alerdn/order-service/app/product"
+	// "github.com/alerdn/order-service/app/user"
+	// "github.com/alerdn/order-service/database"
+	// "github.com/alerdn/order-service/messenger"
 	"github.com/alerdn/order-service/router"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	database.Setup()
-	messenger.Setup()
+	// database.Setup()
+	// messenger.Setup()
 
-	user.Migrate(database.DB)
-	product.Migrate(database.DB)
-	order.Migrate(database.DB)
+	// user.Migrate(database.DB)
+	// product.Migrate(database.DB)
+	// order.Migrate(database.DB)
 
 	server := gin.Default()
 	router.Setup(server)
--- order-service-go/router/router.go
+++ order-service-go/router/router.go
@@ -1,14 +1,20 @@
 package router
 
 import (
-	"github.com/alerdn/order-service/app/order"
-	"github.com/alerdn/order-service/app/product"
-	"github.com/alerdn/order-service/app/user"
+	// "github.com/alerdn/order-service/app/order"
+	// "github.com/alerdn/order-service/app/product"
+	// "github.com/alerdn/order-service/app/user"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Setup(server *gin.Engine) {
-	user.SetupRoutes(server)
-	product.SetupRoutes(server)
-	order.SetupRoutes(server)
+	// user.SetupRoutes(server)
+	// product.SetupRoutes(server)
+	// order.SetupRoutes(server)
+
+	server.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+	})
 }
