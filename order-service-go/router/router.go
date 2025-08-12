e9ce0f778f7dbfea8ab5a8cc6390be4c281f36d6
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2025-08-12 11:05:23 -0400

    wip: subindo todo serviço

--- order-service-go/router/router.go
+++ order-service-go/router/router.go
@@ -1,18 +1,18 @@
 package router
 
 import (
-	// "github.com/alerdn/order-service/app/order"
-	// "github.com/alerdn/order-service/app/product"
-	// "github.com/alerdn/order-service/app/user"
+	"github.com/alerdn/order-service/app/order"
+	"github.com/alerdn/order-service/app/product"
+	"github.com/alerdn/order-service/app/user"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Setup(server *gin.Engine) {
-	// user.SetupRoutes(server)
-	// product.SetupRoutes(server)
-	// order.SetupRoutes(server)
+	user.SetupRoutes(server)
+	product.SetupRoutes(server)
+	order.SetupRoutes(server)
 
 	server.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
