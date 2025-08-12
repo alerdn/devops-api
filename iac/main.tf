d4174170396d4a6f502bafca2a4b0e77c880aaf1
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2025-08-12 09:29:13 -0400

    wip: subindo tag latest

--- iac/main.tf
+++ iac/main.tf
@@ -1,28 +1,28 @@
-module "s3" {
-  source         = "./modules/s3"
-  s3_bucket_name = var.s3_bucket_name
+# module "s3" {
+#   source         = "./modules/s3"
+#   s3_bucket_name = var.s3_bucket_name
 
-  tags = {
-    Iac     = true
-    Context = terraform.workspace
-  }
-}
+#   tags = {
+#     Iac     = true
+#     Context = terraform.workspace
+#   }
+# }
 
-module "cdn" {
-  source             = "./modules/cloudfront"
-  origin_id          = module.s3.bucket_id
-  bucket_domain_name = module.s3.bucket_website_domain_name
-  price_class        = var.cdn_price_class
+# module "cdn" {
+#   source             = "./modules/cloudfront"
+#   origin_id          = module.s3.bucket_id
+#   bucket_domain_name = module.s3.bucket_website_domain_name
+#   price_class        = var.cdn_price_class
 
-  tags = {
-    Iac     = true
-    Context = terraform.workspace
-  }
+#   tags = {
+#     Iac     = true
+#     Context = terraform.workspace
+#   }
 
-  depends_on = [
-    module.s3
-  ]
-}
+#   depends_on = [
+#     module.s3
+#   ]
+# }
 
 module "iam" {
   source = "./modules/iam"
@@ -117,7 +117,7 @@ module "iam" {
   }
 }
 
-module "ecr-repository" {
+module "ecr-repository-invoice" {
   source = "./modules/ecr"
 
   ecr_repository_name                   = "invoice-service-js"
